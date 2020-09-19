8f6b5a2dff2be97245b1833e23baab0734cf1df5
JhonathanJJGS <jhojjgs@gmail.com>
2020-09-19 11:17:53 -0300

    adicionado oled e sensor de temperatura

---
+++ include/sensor_temp.h
@@ -0,0 +1,17 @@
+#include <..\lib\DS18B20\DS18B20.h>
+#include <..\lib\OneWire\OneWire.h>
+
+
+OneWire oneWire(2);
+DS18B20 sensor(&oneWire);
+
+
+
+float getTemperaturaC(){
+    sensor.begin();
+    sensor.requestTemperatures();
+
+    return sensor.getTempC();
+
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <Arduino.h>
 //#include <..\lib\SimpleModBus\SimpleModbusSlave.h>
-#include <..\lib\DS18B20\DS18B20.h>
-#include <..\lib\OneWire\OneWire.h>
-#include <..\lib\U8G2\U8g2lib.h>
 #include <..\lib\HX711\HX711.h>
+#include <..\lib\etherShield\ETHER_28J60\ETHER_28J60.h>
+#include <..\include\display_oled.h>
 
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
 
 
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);
+
+
 HX711 W_CELL;
-OneWire oneWire(4);
-DS18B20 sensor(&oneWire);
+
+
+
+
+
+
 
 
 void setup() {
@@ -25,5 +29,6 @@ void setup() {
 }
 
 void loop() {
-  
+   draw_page();
 }
+
